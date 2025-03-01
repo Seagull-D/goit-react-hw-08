5596dd94fe44fcd2f2a8d93f3aa8fc422e145a64
D-Seagull <dchaika.work@gmail.com>
2025-03-01 17:55:39 +0100

    start auth

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import visibleReducer from "./visibleSlice";
 import { filterReducer } from "./filters/filtersSlice";
 import { contactsReducer } from "./contacts/contactsSlice";
+import { authReduser } from "./auth/authSlice";
 
 export const store = configureStore({
   reducer: {
     contacts: contactsReducer,
     visible: visibleReducer,
     filters: filterReducer,
+    auth: authReduser,
   },
 });
