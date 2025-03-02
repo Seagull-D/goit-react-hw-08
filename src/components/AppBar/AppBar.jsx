ff812144775ea6c953628deeb6021806b57e29d6
D-Seagull <dchaika.work@gmail.com>
2025-03-02 01:08:31 +0100

    logout with navigate

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -2,16 +2,15 @@ import s from "./AppBar.module.css";
 import Navigation from "../Navigation/Navigation";
 import AuthNav from "../AuthNav/AuthNav";
 import { useSelector } from "react-redux";
-import { selectIsLoggedIn, selectUser } from "../../redux/auth/authSelectors";
+import { selectIsLoggedIn } from "../../redux/auth/authSelectors";
 import UserMenu from "../UserMenu/UserMenu";
 
 const AppBar = () => {
-  const user = useSelector(selectUser);
   const isLogin = useSelector(selectIsLoggedIn);
   return (
     <header className={s.container}>
       <h2 className={s.headerName}>Contacts</h2>
-      {user.name && <h2 className={s.headerName}> {user.name}</h2>}
+
       <nav className={s.navList}>
         <Navigation />
         {!isLogin ? <AuthNav /> : <UserMenu />}
